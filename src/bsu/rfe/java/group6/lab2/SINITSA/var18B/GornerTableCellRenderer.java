53336354d10e2f804c98bb86af23acea8d2a1e34
Sinitsa01 <sinitsa2001@inbox.ru>
2020-12-10 01:02:15 +0300

    Lab3_B18_v1

--- src/bsu/rfe/java/group6/lab2/SINITSA/var18B/GornerTableCellRenderer.java
+++ src/bsu/rfe/java/group6/lab2/SINITSA/var18B/GornerTableCellRenderer.java
@@ -6,61 +6,62 @@ import java.awt.FlowLayout;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTable;
+import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
+
 public class GornerTableCellRenderer implements TableCellRenderer {
-private JPanel panel = new JPanel();
-private JLabel label = new JLabel();
-// Ищем ячейки, строковое представление которых совпадает с needle
-// (иголкой). Применяется аналогия поиска иголки в стоге сена, в роли
-// стога сена - таблица
-private String needle = null;
-private DecimalFormat formatter =
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+    private String needle = null;
+    @SuppressWarnings("unused")
+	private String needleTwo = null;
+    private DecimalFormat formatter =(DecimalFormat) NumberFormat.getInstance();
+    public GornerTableCellRenderer() {
+        formatter.setMaximumFractionDigits(5);
+        formatter.setGroupingUsed(false);
+        DecimalFormatSymbols dottedDouble =
+                formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+        panel.add(label);
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    }
+    
+    public Component getTableCellRendererComponent(JTable table,Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+        String formattedDouble = formatter.format(value);
+        label.setText(formattedDouble);
+       
+        if(col==2){
+            double y = (Double) table.getValueAt(row ,1);
+            boolean check;
+            String str = Double.toString(y);
+            String[] Str = str.split("\\.");
+            if (Str[0].equals(Str[1]))check = true;
+            else  check = false;
+          
+            panel.remove(0);
+            JCheckBox jCheckBox = new JCheckBox();
+            jCheckBox.setSelected(check);
+            panel.add(jCheckBox);
+            panel.setBackground(Color.WHITE);
+            return panel;
+        }else{
+            panel.remove(0);
+            panel.add(label);
+        }
+        if(col == 1) {
+            if (needle != null && needle.equals(formattedDouble)) {
+                panel.setBackground(Color.RED);
+            }
+        }
+        return panel;
+    }
 
-(DecimalFormat)NumberFormat.getInstance();
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
 
-public GornerTableCellRenderer() {
-// Показывать только 5 знаков после запятой
-formatter.setMaximumFractionDigits(5);
-// Не использовать группировку (т.е. не отделять тысячи
-// ни запятыми, ни пробелами), т.е. показывать число как "1000",
-// а не "1 000" или "1,000"
-formatter.setGroupingUsed(false);
-// Установить в качестве разделителя дробной части точку, а не
-// запятую. По умолчанию, в региональных настройках
-// Россия/Беларусь дробная часть отделяется запятой
-DecimalFormatSymbols dottedDouble =
-
-formatter.getDecimalFormatSymbols();
-
-dottedDouble.setDecimalSeparator('.');
-formatter.setDecimalFormatSymbols(dottedDouble);
-//Разместить надпись внутри панели
-panel.add(label);
-//Установить выравнивание надписи по левому краю панели
-panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-}
-public Component getTableCellRendererComponent(JTable table,
-Object value, boolean isSelected, boolean hasFocus, int row, int col) {
-//Преобразовать double в строку с помощью форматировщика
-String formattedDouble = formatter.format(value);
-//Установить текст надписи равным строковому представлению числа
-label.setText(formattedDouble);
-if (col==1 && needle!=null && needle.equals(formattedDouble)) {
-//Номер столбца = 1 (т.е. второй столбец) + иголка не null
-//(значит что-то ищем) +
-//значение иголки совпадает со значением ячейки таблицы -
-//окрасить задний фон панели в красный цвет
-panel.setBackground(Color.RED);
-} else {
-//Иначе - в обычный белый
-panel.setBackground(Color.WHITE);
-}
-return panel;
-}
-public void setNeedle(String needle) {
-this.needle = needle;
-}
+    public void setNeedleTwo(String needle) {
+        this.needleTwo= needle;
+    }
 }
